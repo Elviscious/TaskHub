ee5b839ebd09a1432c32fdf303d038369c6fc811
David <dihegaranya@gmail.com>
2025-08-25 17:16:24 +0100

    font-module update

--- app/ownerdashboard/PostJob/page.js
+++ app/ownerdashboard/PostJob/page.js
@@ -9,7 +9,7 @@ export default function PostJob() {
   const [instructions, setInstructions] = useState("");
   const [budget, setBudget] = useState("");
   const [worker, setWorker] = useState("");
-  const [targetCountry, setTargetCountry] = useState("");
+  const [inputLink, setinputLink] = useState("");
 
   const PostJobData = {
     title: jobTitle,
@@ -17,7 +17,7 @@ export default function PostJob() {
     instruction: instructions,
     budget: budget,
     numberOfWorker: worker,
-    country: targetCountry,
+    country: inputLink,
   };
 
   //   useEffect(() => {
@@ -31,123 +31,125 @@ export default function PostJob() {
   };
 
   return (
-    <div>
-      <h1>Post Job</h1>
+		<div className={styles.container}>
+			<h1>Post Job</h1>
 
-      <div className={styles.formContainer}>
-        <form className={styles.jobForm} onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-            <label>Job Title</label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              required
-              value={jobTitle}
-              onChange={(e) => {
-                setJobTitle(e.target.value);
-              }}
-            />
-          </div>
+			<div className={styles.formContainer}>
+				<form className={styles.jobForm} onSubmit={handleSubmit}>
+					<div className={styles.formGroup}>
+						<label>Job Title</label>
+						<input
+							type="text"
+							placeholder="John Doe"
+							required
+							value={jobTitle}
+							onChange={(e) => {
+								setJobTitle(e.target.value);
+							}}
+						/>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Job Type</label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              required
-              value={jobType}
-              onChange={(e) => {
-                setJobType(e.target.value);
-              }}
-            />
-          </div>
+					<div className={styles.formGroup}>
+						<label>Platform</label>
+						<select
+							id="requiredProof"
+							name="requiredProof"
+							className={styles.dropDown}
+						>
+							<option value="">Select Plaform</option>
+							<option value="File">YouTube</option>
+							<option value="screenshot">Instagram</option>
+							<option value="Links">Twitter</option>
+							<option value="Links">Telegram</option> 
+							<option value="Links">Twitch</option> 
+							<option value="Links">App Download</option> 
+						</select>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Instructions</label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              required
-              value={instructions}
-              onChange={(e) => {
-                setInstructions(e.target.value);
-              }}
-            />
-          </div>
+					<div className={styles.formGroup}>
+						<label>Instructions</label>
+						<input
+							type="text"
+							placeholder="Subscribe to my YouTube channel"
+							required
+							value={instructions}
+							onChange={(e) => {
+								setInstructions(e.target.value);
+							}}
+						/>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Required Proof</label>
-            <select
-              id="requiredProof"
-              name="requiredProof"
-              className={styles.dropDown}
-            >
-              <option value="">Select a Proof</option>
-              <option value="File">File</option>
-              <option value="screenshot">Screenshot</option>
-              <option value="Links">Links</option>
-            </select>
-          </div>
+					<div className={styles.formGroup}>
+						<label>Required Proof</label>
+						<select
+							id="requiredProof"
+							name="requiredProof"
+							className={styles.dropDown}
+						>
+							<option value="">Select a Proof</option>
+							<option value="File">File</option>
+							<option value="screenshot">Screenshot</option>
+							<option value="Links">Links</option>
+						</select>
+					</div>
+					<div className={styles.formGroup}>
+						<label>Input Link</label>
+						<input
+							type="text"
+							placeholder="www.youtube.com"
+							value={inputLink}
+							onChange={(e) => {
+								setinputLink(e.target.value);
+							}}
+						/>
+					</div>
+					<div className={styles.formGroup}>
+						<label>Budget</label>
+						<input
+							type="number"
+							placeholder="10,000"
+							required
+							value={budget}
+							onChange={(e) => {
+								setBudget(e.target.value);
+							}}
+						/>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Budget</label>
-            <input
-              type="number"
-              placeholder="John Doe"
-              required
-              value={budget}
-              onChange={(e) => {
-                setBudget(e.target.value);
-              }}
-            />
-          </div>
+					<div className={styles.formGroup}>
+						<label>Number of Workers</label>
+						<input
+							type="number"
+							placeholder="10"
+							required
+							value={worker}
+							onChange={(e) => {
+								setWorker(e.target.value);
+							}}
+						/>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Number of Workers</label>
-            <input
-              type="number"
-              placeholder="John Doe"
-              required
-              value={worker}
-              onChange={(e) => {
-                setWorker(e.target.value);
-              }}
-            />
-          </div>
+					<div className={styles.formCheckBox}>
+						<input type="checkbox" id="deduct" />
+						<label htmlFor="deduct">Deduct total budget from wallet</label>
+					</div>
 
-          <div className={styles.formGroup}>
-            <label>Target Country</label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              value={targetCountry}
-              onChange={(e) => {
-                setTargetCountry(e.target.value);
-              }}
-            />
-          </div>
-
-          <div className={styles.formCheckBox}>
-            <input type="checkbox" id="deduct" />
-            <label htmlFor="deduct">Deduct total budget from wallet</label>
-          </div>
-
-          <div className={styles.formActions}>
-            <button type="button" className={styles.btn}>
-              Save to Draft
-            </button>
-            <button
-              type="submit"
-              className={styles.btn}
-              onClick={() => {
-                console.log(PostJobData);
-              }}
-            >
-              Post Job
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+					<div className={styles.formActions}>
+          <button type="button" className={`${styles.btn} ${styles.draft}`}>
+							Save to Draft
+						</button>
+						<button
+							type="submit"
+							className={styles.btn}
+							onClick={() => {
+								console.log(PostJobData);
+							}}
+						>
+							Post Job
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
