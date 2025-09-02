aef7002c1840b0f6a2358ff0f516b27ad43d354b
David <dihegaranya@gmail.com>
2025-09-02 11:57:04 +0100

    owner dashboard-api

--- app/signin/page.js
+++ app/signin/page.js
@@ -84,11 +84,11 @@ const SignUp = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(signUpData),
-        credentials: "include"
       });
 
       const data = await response.json();
-      // const token = data.Token;  
+      const token = data.Token;  
+      localStorage.setItem("token", token);
 
       if (!response.ok) {
         throw new Error(data.error || "Failed to sign up");
